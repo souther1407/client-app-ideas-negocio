d708ca7c34de4cc91b5fa03f7778be5ce6f20bda
Eric Casanova <kilbane1407@gmail.com>
2023-03-20 16:46:48 -0300

    responsive styles in landing page

--- src/components/organisms/CarouselMultiline/CarouselMultiline.jsx
+++ src/components/organisms/CarouselMultiline/CarouselMultiline.jsx
@@ -9,15 +9,15 @@ const responsive = {
     items: 5,
   },
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
+    breakpoint: { max: 3000, min: 1080 },
     items: 3,
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 3,
+    breakpoint: { max: 1080, min: 640 },
+    items: 2,
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
+    breakpoint: { max: 640, min: 0 },
     items: 1,
   },
 };
