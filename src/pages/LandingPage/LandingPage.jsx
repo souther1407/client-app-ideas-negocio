9d91178b2931b46ddd7dd4906c7d1b59b912c1ce
Eric Casanova <kilbane1407@gmail.com>
2023-04-11 16:58:02 -0300

    fixed landing page redirect

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -5,13 +5,13 @@ import MainBanner from "./components/MainBanner/MainBanner";
 import AiDropshippings from "./components/AiDropshipings/AiDropshipings";
 import { useNavigate } from "react-router-dom";
 import { useLogin } from "../../hooks/useLogin";
-import { DASHBOARD } from "../../utils/constants/routes";
+import { DASHBOARD_IDEAS } from "../../utils/constants/routes";
 
 const LandingPage = () => {
   const navigate = useNavigate();
   const { isLogged } = useLogin({});
   useEffect(() => {
-    if (isLogged()) navigate(DASHBOARD);
+    if (isLogged()) navigate(DASHBOARD_IDEAS);
   }, []);
   return (
     <div className={styles.landingPage}>
