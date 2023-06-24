0ed34e962fe510d6a16f365f46682c83b974fcb0
Eric Casanova <kilbane1407@gmail.com>
2023-06-24 12:46:47 -0300

    add finder page

--- src/pages/Dashboard/components/Finder/Finder.jsx
+++ src/pages/Dashboard/components/Finder/Finder.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./finder.module.css";
+import PlansTable from "../../../../components/organisms/PlansTable/PlansTable";
+import Paginator from "../../../../components/organisms/Paginator/Paginator";
+import { getPublicPrompts } from "../../../../services/userPrompts/getPrompts";
+import Text from "../../../../components/atoms/Text/Text";
+const PlanRow = ({ plan }) => {
+  const { details, input } = plan;
+  return (
+    <>
+      <td>
+        <Text>{details.title}</Text>
+      </td>
+      <td>
+        <Text>{input.location}</Text>
+      </td>
+      <td>
+        <Text>{input.budget}</Text>
+      </td>
+      <td>
+        <Text>15</Text>
+      </td>
+      <td>
+        <Text>algo</Text>
+      </td>
+    </>
+  );
+};
 const Finder = () => {
-  return <div className={styles.finder}>Finder</div>;
+  const [prompts, setPrompts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [rowsNumber, setRowsNumber] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+  useEffect(() => {
+    const initPrompts = async () => {
+      try {
+        const data = await getPublicPrompts();
+        setPrompts(data);
+      } catch (error) {
+        alert(error.message);
+      }
+      setLoading(false);
+    };
+    initPrompts();
+  }, []);
+  const getFilteredPrompsList = () => {
+    const fileted = prompts.slice(
+      (currentPage - 1) * rowsNumber,
+      currentPage * rowsNumber
+    );
+
+    return fileted;
+  };
+  return (
+    <div className={styles.myPrompts}>
+      {loading && <Text>Cargando...</Text>}{" "}
+      {!loading && prompts.length > 0 && (
+        <div className={styles.list}>
+          <Text type="subtitle">My reports</Text>
+          <div className={styles.table}>
+            <PlansTable
+              data={getFilteredPrompsList()}
+              columns={["Title", "Country", "Budget", "adds", "Add"]}
+              renderRow={(data) => <PlanRow plan={data} />}
+            />
+          </div>
+          <div className={styles.paginator}>
+            <Paginator
+              elementsPerPage={rowsNumber}
+              totalElements={prompts.length}
+              onNumPageChange={(value) => setRowsNumber(value)}
+              onPageChange={(page) => setCurrentPage(page)}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Finder;
--- src/pages/Dashboard/components/MyPromps/MyPromps.jsx
+++ src/pages/Dashboard/components/MyPromps/MyPromps.jsx
@@ -4,162 +4,42 @@ import Text from "../../../../components/atoms/Text/Text";
 import { getPrompts } from "../../../../services/userPrompts/getPrompts";
 import PlansTable from "../../../../components/organisms/PlansTable/PlansTable";
 import Paginator from "../../../../components/organisms/Paginator/Paginator";
+import { useNavigate } from "react-router-dom";
+import usePromptDetail from "../../../../states/prompDetail";
+import { formatStringToShort } from "../../../../utils/format/formatStringToShort";
+import IconButton from "../../../../components/molecules/IconButton/IconButton";
+import { MY_PROMPTS_DETAIL } from "../../../../utils/constants/routes";
 
-let id = 0;
-const mockedPromps = [
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-  {
-    id: ++id,
-    input: {},
-    details: {
-      description: "descripcion...",
-      title: `reporte ${id}`,
-    },
-  },
-];
+const PlanRow = ({ plan }) => {
+  const { id, input, details, isPublic } = plan;
+  const navigate = useNavigate();
+  const { setPromptDetail } = usePromptDetail((state) => state);
+  const handleDelete = () => {};
+  const goToPlanDetail = () => {
+    console.log(plan);
+    setPromptDetail({
+      ...plan.details,
+      input: plan.input,
+    });
+    navigate(MY_PROMPTS_DETAIL + `/${id}`);
+  };
+  return (
+    <>
+      <td>
+        <Text onClick={goToPlanDetail}>
+          {formatStringToShort(details.description, 25)}
+        </Text>
+      </td>
+      <td>
+        <Text>{isPublic ? "public" : "private"}</Text>
+      </td>
+      <td>26</td>
+      <td>
+        <IconButton size="16px" icon={"trash"} onClick={handleDelete} />
+      </td>
+    </>
+  );
+};
 
 const MyPromps = () => {
   const [prompts, setPrompts] = useState([]);
@@ -196,7 +76,8 @@ const MyPromps = () => {
           <div className={styles.table}>
             <PlansTable
               data={getFilteredPrompsList()}
-              onRowDelete={(id) => console.log(id)}
+              columns={["Title", "Status", "Views", ""]}
+              renderRow={(data) => <PlanRow plan={data} />}
             />
           </div>
           <div className={styles.paginator}>
